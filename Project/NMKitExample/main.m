e9e6641c1676a7a3a122415e4bd493edb1215124
Владимир Конев <konev.vn@gmail.com>
2014-06-01 20:52:41 +0400

    KoNEW

---
+++ Project/NMKitExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NMKitExample
+//
+//  Created by VLADIMIR KONEV on 01.06.14.
+//  Copyright (c) 2014 Novilab Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NMXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NMXAppDelegate class]));
+    }
+}
